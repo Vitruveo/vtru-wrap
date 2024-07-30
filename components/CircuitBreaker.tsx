341b46c21cc7f2875ba7008d368773b8caddad25
Nik Kalyani <nik@kalyani.com>
2024-07-30 14:23:25 -0700

    Epoch next block number

--- components/CircuitBreaker.tsx
+++ components/CircuitBreaker.tsx
@@ -24,7 +24,7 @@ export default function CircuitBreaker() {
         setLoaded(true);
       const wrapContract = await vitruveoProvider.getContract(WRAP_CONTRACT);
       const cbInfo = await wrapContract.call('circuitBreakerInfo');
-      setNextEpoch(Number(await wrapContract.call('epochNext')));
+      setNextEpoch(Number(await wrapContract.call('epochNextBlock')));
       setPrice(Number(cbInfo[0])/100);
       setTotalEpochWrapLimit(Number(cbInfo[1]));
       setTotalEpochWrapped(Number(cbInfo[5]));
